6c90c2e29cdd9130a0b8fd731f29d3261ef44de3
Lakshmanan K <luxmank@yahoo.com>
2026-07-23 22:51:35 +0530

    Changes to be committed: 	new file:   class1.1/assignment_1.ipynb 	new file:   class1.1/assignment_1.py

---
+++ class1.1/assignment_1.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+
+#Using for loop to iterate through the list
+for i in range(len(fruits)):
+    print(fruits[i])
+
+#Using while loop to iterate through the list
+n=0
+while n in range(len(fruits)):
+    print(fruits[n])
+    n+=1
